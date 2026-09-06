6f6c403f2343df0b4a0a8218a847ad2e1511b417
aditinx <adit.s@inheritx.com>
2026-09-06 23:11:45 +0530

    fixed issues

--- app/restaurants/[resId]/page.js
+++
@@ -1,7 +0,0 @@
-import RestaurantMenu from "../../../src/Components/Menu/RestaurantMenu";
-
-export const dynamic = "force-dynamic";
-
-export default function RestaurantMenuPage() {
-  return <RestaurantMenu />;
-}
